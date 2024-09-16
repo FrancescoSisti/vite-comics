3b515e1d91c71daafb011e259f90c7f6c3aea2c3
FrancescoSisti <francescosisti61@gmail.com>
2024-09-16 14:07:14 +0200

    test commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="ciao">
+        <h1>Ciao ciao ciao</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../../style/variables" as *;
+
+.ciao {
+    background-color: black;
+    color: red;
+    padding: 20px;
+    text-align: center;
+
+    h1 {
+        color: $main-color;
+    }
+}
+</style>
